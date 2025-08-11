ba18b513b47f033ab08d7a81b57f16b72048d32c
Anusmita Misra <anusmita.misra.asn@gmail.com>
2025-08-11 17:58:14 +0530

    Update allowed hosts for render

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,8 @@ SECRET_KEY = 'django-insecure-18=^-@v4lzfj7)y68a(mhx%@4cr*#dd&60_k&7pfxei++8atk3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['restrurant-website-fiqz.onrender.com']
+
 
 # Application definition
 INSTALLED_APPS = [
